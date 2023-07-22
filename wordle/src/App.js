cea765d4909948df8da03c549a07a82305b7f8a7
Rajnish <b421037@iiit-bh.ac.in>
2023-07-22 10:53:25 +0530

    Appropriate comments added for better understanding of the logic of the code

--- wordle/src/App.js
+++ wordle/src/App.js
@@ -5,26 +5,30 @@ import { boardDefault, generateWordSet } from "./Words";
 import React, { useState, createContext, useEffect } from "react";
 import GameOver from "./components/GameOver";
 
+// Create the AppContext to be used for state management across components
 export const AppContext = createContext();
 
 function App() {
-  const [board, setBoard] = useState(boardDefault);
-  const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letter: 0 });
-  const [wordSet, setWordSet] = useState(new Set());
-  const [correctWord, setCorrectWord] = useState("");
-  const [disabledLetters, setDisabledLetters] = useState([]);
+  // State variables used throughout the App component
+  const [board, setBoard] = useState(boardDefault); // The game board
+  const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letter: 0 }); // Current attempt information
+  const [wordSet, setWordSet] = useState(new Set()); // Set of valid words for the game
+  const [correctWord, setCorrectWord] = useState(""); // The correct word that needs to be guessed
+  const [disabledLetters, setDisabledLetters] = useState([]); // List of disabled letters (incorrect and not valid)
   const [gameOver, setGameOver] = useState({
     gameOver: false,
     guessedWord: false,
-  });
+  }); // Game over state information
 
+  // Fetch the word set and the correct word from an external data source using useEffect
   useEffect(() => {
     generateWordSet().then((words) => {
-      setWordSet(words.wordSet);
-      setCorrectWord(words.todaysWord);
+      setWordSet(words.wordSet); // Set the valid word set
+      setCorrectWord(words.todaysWord); // Set the correct word for the game
     });
   }, []);
 
+  // Handler for the Enter key press
   const onEnter = () => {
     if (currAttempt.letter !== 5) return;
 
@@ -32,23 +36,28 @@ function App() {
     for (let i = 0; i < 5; i++) {
       currWord += board[currAttempt.attempt][i];
     }
+
+    // Check if the entered word exists in the word set
     if (wordSet.has(currWord.toLowerCase())) {
-      setCurrAttempt({ attempt: currAttempt.attempt + 1, letter: 0 });
+      setCurrAttempt({ attempt: currAttempt.attempt + 1, letter: 0 }); // Move to the next attempt
     } else {
-      alert("Word not found");
+      alert("Word not found"); // Show an alert if the word is not valid
     }
 
+    // Check if the word is correctly guessed
     if (currWord === correctWord) {
-      setGameOver({ gameOver: true, guessedWord: true });
+      setGameOver({ gameOver: true, guessedWord: true }); // Set game over state as true (successfully guessed the word)
       return;
     }
-    console.log(currAttempt);
+
+    // Check if the player reached the maximum number of attempts (5) and game over
     if (currAttempt.attempt === 5) {
-      setGameOver({ gameOver: true, guessedWord: false });
+      setGameOver({ gameOver: true, guessedWord: false }); // Set game over state as true (failed to guess the word)
       return;
     }
   };
 
+  // Handler for the Delete key press
   const onDelete = () => {
     if (currAttempt.letter === 0) return;
     const newBoard = [...board];
@@ -57,6 +66,7 @@ function App() {
     setCurrAttempt({ ...currAttempt, letter: currAttempt.letter - 1 });
   };
 
+  // Handler for selecting a letter from the keyboard
   const onSelectLetter = (key) => {
     if (currAttempt.letter > 4) return;
     const newBoard = [...board];
@@ -73,6 +83,7 @@ function App() {
       <nav>
         <h1>Wordle</h1>
       </nav>
+      {/* Provide the AppContext to the child components */}
       <AppContext.Provider
         value={{
           board,
@@ -89,7 +100,8 @@ function App() {
         }}
       >
         <div className="game">
-          <Board />
+          <Board /> {/* Render the game board */}
+          {/* Conditionally render either GameOver component or Keyboard component */}
           {gameOver.gameOver ? <GameOver /> : <Keyboard />}
         </div>
       </AppContext.Provider>
--- wordle/src/Words.js
+++ wordle/src/Words.js
@@ -1,5 +1,7 @@
+// Importing the word bank file for generating the word set
 import wordBank from "./wordle-bank.txt";
 
+// Default game board structure with 6 rows and 5 columns
 export const boardDefault = [
   ["", "", "", "", ""],
   ["", "", "", "", ""],
@@ -9,15 +11,20 @@ export const boardDefault = [
   ["", "", "", "", ""],
 ];
 
+// Function to generate a set of words from the word bank file
 export const generateWordSet = async () => {
-  let wordSet;
-  let todaysWord;
+  let wordSet; // A set to store the words from the word bank
+  let todaysWord; // The word randomly selected for the game
+
+  // Fetch the word bank file and process its contents
   await fetch(wordBank)
-    .then((response) => response.text())
+    .then((response) => response.text()) // Get the text content from the response
     .then((result) => {
-      const wordArr = result.split("\n");
-      todaysWord = wordArr[Math.floor(Math.random() * wordArr.length)];
-      wordSet = new Set(wordArr);
+      const wordArr = result.split("\n"); // Split the content into an array of words
+      todaysWord = wordArr[Math.floor(Math.random() * wordArr.length)]; // Select a random word from the array
+      wordSet = new Set(wordArr); // Create a set from the array to ensure unique words
     });
+
+  // Return the generated word set and the word randomly chosen for the game
   return { wordSet, todaysWord };
 };
